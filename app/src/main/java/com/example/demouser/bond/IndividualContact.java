e830dffeb6250c25afd248b1b0d9f742ae672291
Olive <tran22x@mtholyoke.edu>
2018-01-16 13:36:49 -0500

    Added IndividualContact class

---
+++ app/src/main/java/com/example/demouser/bond/IndividualContact.java
@@ -0,0 +1,15 @@
+package com.example.demouser.bond;
+
+/**
+ * Created by Olive on 1/16/18.
+ */
+
+
+public class IndividualContact {
+    protected String name;
+
+    public IndividualContact (String name) {
+        this.name = name;
+    }
+    
+}
