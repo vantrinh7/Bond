e5f7d1cdcee3ca5124bc9b3a832d68c22c9ff7e3
Van Trinh <trinh22v@mtholyoke.edu>
2018-01-17 09:55:31 -0500

    Implemented the IndividualContactActivity, coordinate with parent activity

--- app/src/main/java/com/example/demouser/bond/IndividualContactActivity.java
+++ app/src/main/java/com/example/demouser/bond/IndividualContactActivity.java
@@ -5,16 +5,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class IndividualContactActivity extends AppCompatActivity {
+    public static final String NAME_TEXT = "com.example.demouser.bond.NAME_TEXT";
+    public static final String EMAIL_TEXT = "com.example.demouser.bond.EMAIL_TEXT";
+    public static final String PHONE_TEXT = "com.example.demouser.bond.PHONE_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_contact);
-        String message = getIntent().getStringExtra(MainContactScreen.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        handleSaveButton();
+    }
+
+    /**
+     * Method to determine events when clicking Save button
+     */
+    public void handleSaveButton (){
+        // Find the save button
+        Button saveButton = (Button) findViewById(R.id.button);
+
+        // Add click listener and determine events after click
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get text from name, email, phone text fields, save into variables
+                EditText nameText = (EditText) findViewById(R.id.editText);
+                String name = nameText.getText().toString();
+
+                EditText emailText = (EditText) findViewById(R.id.editText2);
+                String email = emailText.getText().toString();
+
+                EditText phoneText = (EditText) findViewById(R.id.editText3);
+                String phone = phoneText.getText().toString();
+
+                // Create a new intent to hold each data
+                Intent intent = new Intent();
+
+                // Put each of the data along with a "key" name that will be used to get each data
+                intent.putExtra(NAME_TEXT, name);
+                intent.putExtra(EMAIL_TEXT, email);
+                intent.putExtra(PHONE_TEXT, phone);
+
+                // Set the result to be the intent just created
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/demouser/bond/MainContactScreen.java
+++ app/src/main/java/com/example/demouser/bond/MainContactScreen.java
@@ -20,8 +20,8 @@ import java.util.ArrayList;
 
 public class MainContactScreen extends AppCompatActivity {
 
-    // Define a message variable that is the name of the package so that it is unique, in case this app interacts with other apps
-    public static final String EXTRA_MESSAGE = "com.example.demouser.bond.MESSAGE";
+    public static final int REQUEST_CODE_INDIVIDUAL_CONTACT_ACTIVITY = 0;
+
     private String m_Text = "";
     private ArrayList<String> values = new ArrayList<>();
     private ArrayAdapter<String> adapter;
@@ -44,30 +44,60 @@ public class MainContactScreen extends AppCompatActivity {
             }
         });
 
-
         ListView contactList = (ListView) findViewById(R.id.contactList);
 
         values.add("Mary Lyon");
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+        // Define a new Adapter: First parameter - Context, Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written, Forth - the Array of data
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
         // Assign adapter to ListView
         contactList.setAdapter(adapter);
-
     }
 
+    /**
+     * Method to determine events when add button is clicked
+     */
     public void initiateAddButton() {
+        // Create a new intent to start a new activity. First parameter: context, usually this.
+        // Second parameter: the class that denotes new activity
         Intent intent = new Intent(this, IndividualContactActivity.class);
-        String message = "This is a message";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        // Use startActivity() to simply start a new screen, use startActivityForResult() to get some results back.
+        // Must implement onActivityResult() method below to denote what happens when result comes back
+        // First parameter - the new intent, second parameter - the code that identifies where the result comes from
+        startActivityForResult(intent, REQUEST_CODE_INDIVIDUAL_CONTACT_ACTIVITY);
     }
+
+    /**
+     * Method that determines what happens when result comes back from the child activity
+     * @param requestCode supplied by startActivityForResult(), it identifies who this result came from
+     * @param resultCode the integer result code returned by the child activity through its setResult()
+     * @param data an intent that holds the data to be returned
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // If the request code is the code for IndividualContactActivity
+        if (REQUEST_CODE_INDIVIDUAL_CONTACT_ACTIVITY == 0) {
+
+            // If the result code matches
+            if (resultCode == RESULT_OK) {
+
+                // Get the name, email, phone data from the IndividualContactActivity class
+                String name = data.getStringExtra(IndividualContactActivity.NAME_TEXT);
+                String email = data.getStringExtra(IndividualContactActivity.EMAIL_TEXT);
+                String phone = data.getStringExtra(IndividualContactActivity.PHONE_TEXT);
+
+                // Change the ArrayAdapter and display
+                values.add(name + "  " + email + "  " + phone);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
