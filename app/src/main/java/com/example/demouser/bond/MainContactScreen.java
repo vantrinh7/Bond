79f61e312090039905980450ebcd7dda3ca89b45
Yoojin Kwon <yoojink131@gmail.com>
2018-01-18 14:14:32 -0500

    Customized Calendar

--- app/src/main/java/com/example/demouser/bond/MainContactScreen.java
+++ app/src/main/java/com/example/demouser/bond/MainContactScreen.java
@@ -1,19 +1,16 @@
 package com.example.demouser.bond;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -35,6 +32,7 @@ public class MainContactScreen extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id)
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,6 +42,15 @@ public class MainContactScreen extends AppCompatActivity {
             }
         });
 
+        Button calendarButton = findViewById(R.id.button);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                initiateCalendarButton();
+
+            }
+        });
         ListView contactList = (ListView) findViewById(R.id.contactList);
 
         values.add("Mary Lyon");
@@ -57,6 +64,11 @@ public class MainContactScreen extends AppCompatActivity {
         contactList.setAdapter(adapter);
     }
 
+    public void initiateCalendarButton(){
+        Intent intent = new Intent(this, CalendarMainActivity.class);
+
+        startActivity(intent);
+    }
     /**
      * Method to determine events when add button is clicked
      */
@@ -81,7 +93,7 @@ public class MainContactScreen extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         // If the request code is the code for IndividualContactActivity
-        if (REQUEST_CODE_INDIVIDUAL_CONTACT_ACTIVITY == 0) {
+        if (requestCode == REQUEST_CODE_INDIVIDUAL_CONTACT_ACTIVITY) {
 
             // If the result code matches
             if (resultCode == RESULT_OK) {
